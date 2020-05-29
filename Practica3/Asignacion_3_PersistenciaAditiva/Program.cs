def16f26c1d33ad7ac1921ec78390c3ec0fd3937
Jjwot-Dud3 <62677461+Jjwot-Dud3@users.noreply.github.com>
2020-05-28 20:53:57 -0400

    Practica3

---
+++ Practica3/Asignacion_3_PersistenciaAditiva/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Asignacion_3_PersistenciaAditiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la longitud");
+            String result = Console.ReadLine();
+
+            string[] intList = result.Select(q => new string(q, 1)).ToArray();
+            int[] myInts = Array.ConvertAll(intList, int.Parse);
+
+            int count = 0;
+            while (myInts.Length > 1)
+            {
+                int sum = 0;
+                for (int i = 0; i < myInts.Length; i++)
+                {
+                    sum += myInts[i];
+
+                }
+
+                string[] arr = Convert.ToString(sum).Select(q => new string(q, 1)).ToArray();
+                myInts = Array.ConvertAll(arr, int.Parse);
+                count++;
+            }
+            Console.WriteLine(count);
+
+            Console.ReadKey();
+        }
+    }
+}
